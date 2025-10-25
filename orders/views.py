aa0572d28ca817431515b493afcadfd56da8fded
Asad-max-jpg <S310990@uos.ac.uk>
2025-10-25 19:44:44 +0500

    Fix PEP8 issues in orders/views.py for CI

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Order, OrderItem
-from products.models import Product
+from .models import Order
 from django.views.decorators.http import require_http_methods
 
 
@@ -8,18 +7,22 @@ from django.views.decorators.http import require_http_methods
 def order_create(request):
     if not request.user.is_authenticated:
         return redirect('login')
-        
+
     if request.method == "POST":
         email = request.POST.get('email')
-        order = Order.objects.create(
+        # The order object isn't used, so prefix with underscore to indicate it's intentional
+        _ = Order.objects.create(
             email=email,
             user=request.user
         )
         return redirect('orders:list')
+
     return render(request, 'orders/order_form.html')
+
 
 def order_list(request):
     if not request.user.is_authenticated:
         return redirect('login')
+
     orders = Order.objects.filter(user=request.user).order_by('-created_at')
     return render(request, 'orders/order_list.html', {'orders': orders})
